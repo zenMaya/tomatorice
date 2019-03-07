bb05a37caee86cc553ef8d2a5b43049d77090240
Jáchym Tomášek <jachym.tomasek@outlook.com>
2019-03-07 10:10:55 +0100

    Do some cleaning and stuff

--- .config/emacs/config.el
+++ .config/emacs/config.el
@@ -49,6 +49,8 @@
   (add-hook 'org-mode-hook (lambda ()
      (org-bullets-mode 1))))
 
+(use-package flycheck)
+
 (use-package rust-mode)
 
 (use-package cargo
@@ -162,21 +164,3 @@
 ;;  :ensure t
 ;;  :options
 ;;  (setq mail-user-agent 'mu4e-user-agent))
-;;-----------------------------------------------------------------------------
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ede-project-directories
-   (quote
-    ("/home/yachimm_thomasegh/projects/cadump-compiler")))
- '(package-selected-packages
-   (quote
-    (org-bullets which-key flycheck-clangcheck flycheck-clang-tidy flycheck-clang-analyzer flycheck lsp-ui git-gutter treemacs-magit treemacs-projectile cmake-ide cmake-font-lock ccls cmake-mode doom-themes doom-modeline company helm-projectile projectile helm treemacs move-text use-package hydra))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
